e3fb0a3eaf2494c570bd4e3baa78a0f5675d2705
Zaland Khan <zaland@bluenose.cs.dal.ca>
2016-03-19 20:26:09 -0300

    added navbar img text

--- index.php
+++ index.php
@@ -4,6 +4,12 @@
 ?>
 
         <img id="navbar-img" src="img/navbar-img.jpg" class="img-responsive" alt="Image">
-
-
+        
+        <div class="container">
+            <div class="col-lg-8 col-lg-offset-2">
+                <h1 id="navbar-title"> ONE OF THE BEST SOCIAL MEDIA WEBSITES YOU WILL LOVE! </h1>
+                <p id="navbar-text"> Our word, or your money back, GUARANTEED! </p>
+            </div>
+        </div>
+        
 <?php require("content/footer.php"); ?>
