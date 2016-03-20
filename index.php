14eaf22f8d5f6670e779c3932023f7c85a8b39b0
Zaland Khan <zaland@bluenose.cs.dal.ca>
2016-03-20 01:30:10 -0300

    added info in index

--- index.php
+++ index.php
@@ -9,7 +9,27 @@
             <div class="col-lg-8 col-lg-offset-2">
                 <h1 id="navbar-title"> ONE OF THE BEST SOCIAL MEDIA WEBSITES YOU WILL LOVE! </h1>
                 <p id="navbar-text"> Our word, or your money back, GUARANTEED! </p>
+                <p id="navbar-link"><a href="#"> Sign Up Now </a></p>
             </div>
+            
+            <div class="row">
+                <div class="col-lg-4 clearfix info-text">
+                    <img src="img/icons/hourglass.png" class="img-responsive info-img" alt="Image">
+                    <h3> Addicting </h3>
+                    <p> You will fall in love with this website. You will need a break but will not want to take one. Use in moderation. </p>
+                </div>
+                <div class="col-lg-4 clearfix info-text">
+                    <img src="img/icons/money.png" class="img-responsive info-img" alt="Image">
+                    <h3> Free </h3>
+                    <p> Absolutely Free. You don't have to pay us a single dime. Just register, login, and have fun. </p>
+                </div>
+                <div class="col-lg-4 clearfix info-text">
+                    <img src="img/icons/locked.png" class="img-responsive info-img" alt="Image">
+                    <h3> Secure </h3>
+                    <p> You can trust us. Our website offers the latest secrutiy measure to keep your data out of prying hands. </p>
+                </div>
+            </div>
+            <br>
         </div>
         
-<?php require("content/footer.php"); ?>
+<?php require("content/footer.html"); ?>
